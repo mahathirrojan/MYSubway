17f0a9c14908518394dd319726eece02e213b75c
mahathirrojan <torbengalifreind@gmail.com>
2024-11-10 23:18:04 -0500

    Initial Commit

---
+++ MYSubway/MYSubwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYSubwayApp.swift
+//  MYSubway
+//
+//  Created by Mahathir Rojan on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MYSubwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
